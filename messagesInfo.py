c468aa618f134e642207a1deae35002eee1f4584
Damian <c.damiankucharski@gmail.com>
2018-12-17 15:46:40 +0100

    change getUsers and add getMessageData function

--- messagesInfo.py
+++ messagesInfo.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import glob
-
+import pandas as pd
+from operator import itemgetter
+from datetime import datetime as dt
 
 def getAllFiles(path=""):
     files = glob.glob(path + "*html")
@@ -20,15 +22,24 @@ def combineFiles(list, outfileName="result.html"):
     return outfile
 
 
-def getUsers(file):
+def getUsers(soup):
     seen_users = set()
 
     def users(tag):
         username = tag.get('title')
-        if username and 'message' in tag.get('class', ''):
+        if username and ('box_r' in tag.get('class', '') or 'box_l' in tag.get('class', '')):
             seen_users.add(username)
             return True
 
-    soup = BeautifulSoup(file, 'html.parser')
     soup.find_all(users)
     return seen_users
+
+
+def getMessageData(file):
+    data = []
+    soup = BeautifulSoup(file, 'html.parser')
+    for name in getUsers(soup):
+        for div in soup.find_all("div",{"title":name}):
+            data.append([name,dt.strptime(div["time"],"%m/%d/%Y %I:%M:%S %p").strftime("%m/%d/%Y"),div.find('span').text])
+
+    return data
